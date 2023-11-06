db4ce52269a57588cbc0efa77d4c83f40bfca7c6
hydroakri <hsx011231@gmail.com>
2023-11-06 17:58:41 +0800

    remove safetynet

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,8 +1,3 @@
 #!/system/bin/sh
-
-# Remove Play Services from the Magisk Denylist when set to enforcing.
-if magisk --denylist status; then
-	magisk --denylist rm com.google.android.gms
-fi
 echo 1 > /sys/kernel/oplus_display/dimlayer_hbm
 echo 1 > /sys/kernel/oplus_display/dc_real_backlight
