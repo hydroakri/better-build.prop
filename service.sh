fcfe4fdcb277c6362d4e911356d8c375b94f63ee
hydroakri <hsx011231@gmail.com>
2024-03-03 12:35:21 +0800

    add app freezer

--- service.sh
+++ service.sh
@@ -2,7 +2,7 @@
 hour=$(date +'%H')
 while :; do
     if [ $hour -eq 04 ]; then
-        device_config put activity_manager_native_boot use_freezer true
+        # device_config put activity_manager_native_boot use_freezer true
         device_config put activity_manager_native_boot freeze_debounce_timeout 5000
         cmd package bg-dexopt-job
         cmd package compile -m everything -a
